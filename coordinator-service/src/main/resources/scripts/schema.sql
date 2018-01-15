cdfc33f3db8406db747da3f3a3e44be8ad679984
Leonan Teixeira <leonan.teixeira@gmail.com>
2018-01-15 01:42:42 -0300

    update coordinator schema.sql

--- coordinator-service/src/main/resources/scripts/schema.sql
+++ coordinator-service/src/main/resources/scripts/schema.sql
@@ -1,11 +1,14 @@
 # CREATE DATABASE `smartlab_coordinator` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-CREATE TABLE `smartlab_coordinator`.`vote` (
-  `id_vote` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NULL,
-  `online_users` INT NULL,
-  `external_temperature` FLOAT NULL,
-  `internal_temperature` FLOAT NULL,
-  `vote` INT NULL,
+CREATE TABLE `vote` (
+  `id_vote` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `online_users` int(11) DEFAULT NULL,
+  `external_temperature` float DEFAULT NULL,
+  `internal_temperature` float DEFAULT NULL,
+  `vote` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `hour` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_vote`),
-  UNIQUE INDEX `id_vote_UNIQUE` (`id_vote` ASC));
+  UNIQUE KEY `id_vote_UNIQUE` (`id_vote`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
